5d95e36042b5283638c578ee38ba329ff3911c7f
Rosanna Fong <me@rosanna.co.nz>
2016-07-02 18:17:16 +1200

    Uploaded new files for portfolio version 6.0.0

--- js/hide-message.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-	if (jQuery("#message").hasClass("slideup"))
-    	jQuery("#message").removeClass("slideup").addClass("slidedown");
-  	else
-    	jQuery("#message").removeClass("slidedown").addClass("slideup");
-    jQuery('.hide').click(function(){
-      	jQuery("#message").removeClass("slidedown").addClass("slideup");
-    });
-});
--- js/menu-toggle.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-    jQuery('.toggle-btn').click(function(e) {
-        jQuery(this).toggleClass('active');
-        jQuery('.primary-nav ul').toggleClass('toggle-nav');
- 
-        e.preventDefault();
-    });
-});
